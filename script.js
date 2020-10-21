246ebb15b463607e4d91d7f77e9aaeccb945919b
Ulik <example@gmail.com>
2020-10-21 17:21:18 +0200

    if statement and loops learned

--- script.js
+++ script.js
@@ -32,13 +32,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++){
+    const  a = prompt('Один из последних просмотренных фильмов?', ''),
+           b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+           
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10){
+    console.log("Malo prosmotreno")
+} else if (personalMovieDB.count >=10 && personalMovieDB.count < 30 ) {
+    console.log('Classic viewer')
+} else if (personalMovieDB.count >= 50) {
+    console.log('Cool viewer')
+} else {
+    console.log('Error')
+}
 
 console.log(personalMovieDB);
  
